e886f8df7c4449ad33ed1ae6859553500149bcc6
Daniel Chen <danielchen1009@berkeley.edu>
2023-07-24 19:51:25 -0700

    founder page almost done

--- src/routes/founder/+page.svelte
+++ src/routes/founder/+page.svelte
@@ -4,7 +4,7 @@
 <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
 
 <script>
-
+    let h1, h2, h3, h4, h5, h6;
 </script>
 
 <div class="banner bannersize">
@@ -30,17 +30,101 @@
     <div class="dot orange center" style="top: 240vh;"></div>
     <div class="dot black center" style="top: 280vh;"></div>
 
-    <div class="entrycontainer centerleft" style="top: 34vh">
+    <div class="entrycontainer centerleft" style="top: {document.documentElement.clientHeight * 0.405 - h1/2}px;">
         <div class="age">
-            Age 0
+            Age 11
         </div>
-        <div class="content">
+        <div class="content" bind:clientHeight={h1}>
             Alan Zhang was born in 2004 in Guangzhou, China. At the young age of 11,
             he sparked an interest in making DIY kits after learning to use a soldering
             iron and glue gun.
         </div>
         <div style="width: 100%; display:flex; justify-content: right">
-            <div class="timeimgwrapper">
+            <div class="timeimgwrapper" style="justify-content: right;">
+                <img src="./src/imgs/founderimgs/p1.jpeg" class="timelineimg">
+            </div>
+        </div>
+    </div>
+    <div class="entrycontainer centerright" style="top: {document.documentElement.clientHeight * 0.805 - h2/2}px;">
+        <div style="width: 100%; display:flex; justify-content: right">
+            <div class="age">
+                Age 13
+            </div>
+        </div>
+        <div class="content" bind:clientHeight={h2}>
+            Two years later, at age 13: he assembled his first toy drone
+            and GoPro to shoot into the skies. Upon being gifted a DJI Phantom 4,
+            his interest in drone aviation soon became an obsession.
+        </div>
+        <div style="width: 100%; display:flex; justify-content: left">
+            <div class="timeimgwrapper" style="justify-content: left;">
+                <img src="./src/imgs/founderimgs/p2.png" class="timelineimg">
+            </div>
+        </div>
+    </div>
+    <div class="entrycontainer centerleft" style="top: {document.documentElement.clientHeight * 1.205 - h3/2}px;">
+        <div class="age">
+            Age 14
+        </div>
+        <div class="content" bind:clientHeight={h3}>
+            Alan built his first FPV drone a year later (14),
+            and competed in the BIEA International STEM Competition for drone
+            design, where he won as an International Finalist and recipient of the
+            Most Creative Award.
+        </div>
+        <div style="width: 100%; display:flex; justify-content: right">
+            <div class="timeimgwrapper" style="justify-content: right;">
+                <img src="./src/imgs/founderimgs/p3.png" class="timelineimg">
+            </div>
+        </div>
+    </div>
+    <div class="entrycontainer centerright" style="top: {document.documentElement.clientHeight * 1.605 - h4/2}px;">
+        <div style="width: 100%; display:flex; justify-content: right">
+            <div class="age">
+                Age 15
+            </div>
+        </div>
+        <div class="content" bind:clientHeight={h4}>
+            At age 15, he co-founded “FPV Creator,” a VC-backed startup what specialized
+            in designing drones, drop-shipping parts, and helping others share his own passion.
+            While doing so, he quickly accumulated over 30,000 online followers on Instagram and YouTube.
+        </div>
+        <div style="width: 100%; display:flex; justify-content: left">
+            <div class="timeimgwrapper" style="justify-content: left;">
+                <img class="timelineimg">
+            </div>
+        </div>
+    </div>
+    <div class="entrycontainer centerleft" style="top: {document.documentElement.clientHeight * 2.005 - h5/2}px;">
+        <div class="age">
+            Age 16
+        </div>
+        <div class="content" bind:clientHeight={h5}>
+            During high school, Alan launched a drone club at his high school, named Project Leelyn Drone,
+            fundraising over $25,000 and led the world’s first team of high school students in engineering
+            a human-carrying electric aircraft.
+        </div>
+        <div style="width: 100%; display:flex; justify-content: right">
+            <div class="timeimgwrapper" style="justify-content: right;">
+                <img src="./src/imgs/founderimgs/p5.png" class="timelineimg">
+            </div>
+        </div>
+    </div>
+    <div class="entrycontainer centerright" style="top: {document.documentElement.clientHeight * 2.405 - h6/2}px;">
+        <div style="width: 100%; display:flex; justify-content: right">
+            <div class="age">
+                Present
+            </div>
+        </div>
+        <div class="content" bind:clientHeight={h6}>
+            Today, Alan Zhang attends the University of California, Berkeley as a student within the Management,
+            Entrepreneurship, and Technology (M.E.T) Program; he double majors in Mechanical Engineering and Business
+            Administration. Apart from joining Berkeley’s Aviation Innovation Research Lab, he launched his own
+            student-run VTOL organization: VTOL @ Berkeley. He’s been able to deeply discuss topics in aviation
+            and eVTOL with the founders of Opener and XPeng AeroHT, both industrial leaders in the field.
+        </div>
+        <div style="width: 100%; display:flex; justify-content: left">
+            <div class="timeimgwrapper" style="justify-content: left;">
                 <img src="./src/imgs/founderimgs/p1.jpeg" class="timelineimg">
             </div>
         </div>
@@ -56,7 +140,6 @@
         width: 60%;
         background-color: #FF2700;
         margin: 0;
-        justify-content: right;
     }
     .centerleft {
         left: 4vw;
