bb842e331361debbb71c1b54d93b8fbe0c7ed4ac
Daniel Chen <danielchen1009@berkeley.edu>
2023-08-16 12:49:05 -0700

    obscurized env

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -3,6 +3,7 @@ import { default as mongodb } from 'mongodb';
 const MongoClient = mongodb.MongoClient;
 import { DB_URI } from '$env/static/private'
 const uri = DB_URI;
+// const uri = process.env.DB_URI;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri);
 
