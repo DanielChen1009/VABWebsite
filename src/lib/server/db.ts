cf74412346d51d78960cf06ab51701950c7ac399
Daniel Chen <danielchen1009@berkeley.edu>
2023-08-16 12:53:12 -0700

    actually obscurized env

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -1,9 +1,9 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 import { default as mongodb } from 'mongodb';
 const MongoClient = mongodb.MongoClient;
-import { DB_URI } from '$env/static/private'
-const uri = DB_URI;
-// const uri = process.env.DB_URI;
+// import { DB_URI } from '$env/static/private'
+// const uri = DB_URI;
+const uri = process.env.DB_URI;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri);
 
